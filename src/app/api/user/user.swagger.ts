9404abf408a546eb8b419f3f525cdb14e528c4f9
Shahid Tumbi <dev@nksqr.com>
2023-09-06 18:23:13 +0530

    remove unnecessary code

--- src/app/api/user/user.swagger.ts
+++ src/app/api/user/user.swagger.ts
@@ -1,7 +1,6 @@
 /* tslint:disable:max-classes-per-file */
 
 import { ApiModel, ApiModelProperty, SwaggerDefinitionConstant } from 'swagger-express-ts';
-import { UserStatus } from '@src/app/constants';
 // import { IPoint } from '../api.interface';
 
 // export * from './banner/banner.swagger';
@@ -45,98 +44,6 @@ export class ForgotPasswordData {
 }
 
 @ApiModel({
-	name: 'ResetPasswordData',
-	description: '',
-})
-export class ResetPasswordData {
-	@ApiModelProperty({
-		description: 'JWT Token received from verify api(/forget-password/verify)',
-		required: true,
-		type: SwaggerDefinitionConstant.STRING,
-		example: 'JWT TOKEN' as any,
-	})
-	token: string;
-	@ApiModelProperty({
-		description: 'New Password',
-		required: true,
-		type: SwaggerDefinitionConstant.STRING,
-		example: '123456' as any,
-	})
-	password: string;
-}
-
-@ApiModel({
-	name: 'VerifyForgetOtp',
-	description: '',
-})
-export class VerifyForgetOtp {
-	@ApiModelProperty({
-		description: 'OTP Code',
-		required: true,
-		type: SwaggerDefinitionConstant.STRING,
-		example: '1234' as any,
-	})
-	otp: string;
-	@ApiModelProperty({
-		description: 'JWT Token received from forget api(/forget-password)',
-		required: true,
-		type: SwaggerDefinitionConstant.STRING,
-		example: 'JWT TOKEN' as any,
-	})
-	token: string;
-}
-
-@ApiModel({
-	name: 'ResendPasswordOtp',
-	description: '',
-})
-export class ResendPasswordOtp {
-	@ApiModelProperty({
-		description: 'JWT Token received from forget api(/forget-password)',
-		required: true,
-		type: SwaggerDefinitionConstant.STRING,
-		example: 'JWT TOKEN' as any,
-	})
-	token: string;
-}
-
-
-@ApiModel({
-	name: 'FollowData',
-	description: '',
-})
-export class FollowData {
-	userId!: string;
-	@ApiModelProperty({
-		description: 'Follow Status (true: follow | false: unfollow)',
-		required: false,
-		type: SwaggerDefinitionConstant.BOOLEAN,
-	})
-	status!: boolean;
-}
-
-@ApiModel({
-	description: 'User Profile Status Update',
-	name: 'UserProfileStatusUpdate',
-})
-export class UserProfileStatusUpdate {
-	@ApiModelProperty({
-		description: Object.entries(UserStatus).map((val) => val.join(': ')).join(' | '),
-		required: false,
-		type: SwaggerDefinitionConstant.STRING,
-		example: Object.values(UserStatus).join(' | ') as any,
-	})
-	status: string;
-	@ApiModelProperty({
-		description: 'Id of User',
-		required: false,
-		type: SwaggerDefinitionConstant.STRING,
-		example: '5erre5-refd45yuyu-dsdsfd-43fdd' as any,
-	})
-	id: string;
-}
-
-@ApiModel({
 	description: 'Change Password',
 	name: 'ChangePasswordData',
 })
@@ -164,52 +71,6 @@ export class PasswordData {
 	new_password: string;
 }
 
-@ApiModel({
-	name: 'ReportUser',
-	description: 'Report a User',
-})
-export class ReportUserData {
-	@ApiModelProperty({
-		type: SwaggerDefinitionConstant.STRING,
-		required: true,
-		description: 'Reason ID',
-	})
-	reason_id!: string;
-	@ApiModelProperty({
-		type: SwaggerDefinitionConstant.STRING,
-		required: false,
-		description: 'Report Description',
-	})
-	description?: string;
-	user_id!: string;
-}
-
-@ApiModel({
-	name: 'DeviceToken',
-	description: 'Device Token Data',
-})
-export class DeviceTokenData {
-	@ApiModelProperty({
-		type: SwaggerDefinitionConstant.STRING,
-		required: true,
-		description: 'Device Token',
-	})
-	device_token!: string;
-}
-
-@ApiModel({
-	name: 'UpdatePrivacy',
-	description: 'Update Privacy Data',
-})
-export class UpdatePrivacyData {
-	@ApiModelProperty({
-		type: SwaggerDefinitionConstant.BOOLEAN,
-		required: true,
-		description: 'Privacy',
-		example: false as any,
-	})
-	is_private!: boolean;
-}
 @ApiModel({
 	name: 'UpdateUserData',
 	description: 'Update User Data',
--- src/swaggermodels/index.ts
+++ src/swaggermodels/index.ts
@@ -1,4 +1,3 @@
 export * from './user.swagger.model';
 // export * from '@api/api.swagger';
 export * from '@api/api.swagger';
-export * from './admin.swagger.model';
